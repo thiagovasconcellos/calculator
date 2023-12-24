4ddcda4e99fa328d56d864020513f7622eb16445
Thiago Anuszkiewicz <tcunha@openskydata.com>
2023-12-24 15:54:14 +0000

    centering

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,27 @@ import Calculator from "./Calculator"
 
 function App() {
   return (
-    <div style={{ width: '400px', height: '600px', margin: 'auto', border: '1px solid #ccc', borderRadius: '10px', overflow: 'hidden' }}>
-      <Calculator />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "400px",
+          height: "600px",
+          border: "1px solid #ccc",
+          borderRadius: "10px",
+          overflow: "hidden",
+        }}
+      >
+        <Calculator />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
